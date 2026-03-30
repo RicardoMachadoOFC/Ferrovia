a2bb53afebe5a4f89df655a35d6fa89b0866795f
BrunoMontanari1303 <bruno.s.montanari@outlook.com>
2026-03-30 09:09:35 -0300

    Implementação do Kruskal

--- src/main/java/net/javaguides/FerroviaApp.java
+++ src/main/java/net/javaguides/FerroviaApp.java
@@ -1,5 +1,14 @@
+package net.javaguides;
+import net.javaguides.springboot.controller.GrafoController;
+import net.javaguides.springboot.controller.KruskalController;
+import net.javaguides.springboot.model.Grafo;
+
+
 public class FerroviaApp {
     public static void main(String[] args) {
-        
+        GrafoController grafoController = new GrafoController();
+        Grafo grafo = grafoController.criarGrafo();
+        KruskalController kruskalController = new KruskalController();
+        kruskalController.ordenarArestas(grafo);
     }
 }

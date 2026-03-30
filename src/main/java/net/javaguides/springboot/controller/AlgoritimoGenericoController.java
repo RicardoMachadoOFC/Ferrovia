cc7413c7ae1a8644007cc6a8d2b57d9f501cd205
StefaniPm <stefanipires.m@gmail.com>
2026-03-30 10:52:24 -0300

    Base algoritmo genético

--- src/main/java/net/javaguides/springboot/controller/AlgoritimoGenericoController.java
+++ src/main/java/net/javaguides/springboot/controller/AlgoritimoGenericoController.java
@@ -7,12 +7,20 @@ import net.javaguides.springboot.model.Aresta;
 import net.javaguides.springboot.model.Ferrovias;
 
 public class AlgoritimoGenericoController {
+    
+    class Individuo {
+    int[] genes;
+    double fitness;
+
+}
 
     public List<Ferrovias> otimizarFerrovias(List<Aresta> possiveis, double orcamento){
+    
 
         List<Ferrovias> melhorSolucao = new ArrayList<>();
 
-        // população inicial
+
+       
         // seleção
         // crossover
         // mutação
@@ -20,4 +28,30 @@ public class AlgoritimoGenericoController {
 
         return melhorSolucao;
     }
+    //cada individuo será uma solução 
+    //Serve para termos a população incial 
+    public Individuo criarIndividuo(int tamanho) {
+        Individuo ind = new Individuo();
+        ind.genes = new int[tamanho];
+        
+        //Vai preencher o array de genes com valores aleatorios entre 0 ou 1
+        for (int i = 0; i < tamanho; i++){
+            ind.genes[i] = Math.random() < 0.5 ? 1 : 0; //se for menor que 0,5 vai ser 1 maior 0
+        }
+        
+        return ind;
+    }
+ 
+    //população é o conjunto de soluções
+    //Serve para criar um conjunto (população) de combinações de ferrovias possiveis 
+    public List<Individuo> criarPopulacao(int tamanhoPop, int tamanhoGenes){
+        List<Individuo> pop = new ArrayList<>();
+
+        for (int i = 0; i < tamanhoPop; i++){
+            pop.add(criarIndividuo(tamanhoGenes));
+        }
+        return pop;
+    }
+    
+    
 }
